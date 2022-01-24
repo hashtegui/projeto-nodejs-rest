42aad4d4203c18c022cc1b4808772362728bdfce
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 15:06:47 -0400

    criando a conexao com o banco de dados

--- config/customExpress.js
+++ config/customExpress.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser')
 module.exports = () => {
     const app = express()
 
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.use(bodyParser.json({extended:true}))
+    app.use(express.urlencoded({extended:true}))
+    app.use(express.json({extended:true}))
     consign()
         .include('controllers')
         .into(app)
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const customExpress = require('./config/customExpress.js')
+const conexao = require('./infraestrutura/conexao')
 
-app = customExpress()
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log("Banco de dados iniciado com sucesso")
+        app = customExpress()
 
-app.listen(3000, () => console.log("servidor rodando na porta 3000"))
+        app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
+    }
+})
+
 
