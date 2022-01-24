60e3cab38a0b6bed198a2a53650a1e36eb085186
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 17:56:33 -0400

    adicionando e tratando datas

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -7,7 +7,7 @@ class Tabelas{
     }
 
     criarAtendimento(){
-        const sql = 'CREATE TABLE if not exists atendimentos (id int not null auto_increment, cliente varchar(50)not null, pet varchar(20), servico varchar(20) not null, status varchar(20) not null, observacoes text, primary key(id))'
+        const sql = 'CREATE TABLE if not exists atendimentos (id int not null auto_increment, cliente varchar(50)not null, pet varchar(20), servico varchar(20) not null, status varchar(20) not null, observacoes text, data datetime not null, datacriacao datetime not null,primary key(id))'
 
         this.conexao.query(sql, (erro)=>{
             if(erro){
