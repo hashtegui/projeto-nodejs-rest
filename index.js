90d3986d4b99307de9762e6950a248e42a5e8e65
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 17:29:01 -0400

    criando tabelas no banco dadados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress.js')
 const conexao = require('./infraestrutura/conexao')
+const Tabelas= require('./infraestrutura/tabelas')
 
 conexao.connect((erro) => {
     if (erro) {
@@ -7,7 +8,7 @@ conexao.connect((erro) => {
     } else {
         console.log("Banco de dados iniciado com sucesso")
         app = customExpress()
-
+        Tabelas.init(conexao)
         app.listen(3000, () => console.log("Servidor rodando na porta 3000"))
     }
 })
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,22 @@
+const e = require("express")
+
+class Tabelas{
+    init(conexao){
+        this.conexao = conexao
+        this.criarAtendimento()
+    }
+
+    criarAtendimento(){
+        const sql = 'CREATE TABLE if not exists atendimentos (id int not null auto_increment, cliente varchar(50)not null, pet varchar(20), servico varchar(20) not null, status varchar(20) not null, observacoes text, primary key(id))'
+
+        this.conexao.query(sql, (erro)=>{
+            if(erro){
+                console.log(erro)
+            }else{
+                console.log("Tabela atendimento criada")
+            }
+        })
+    }
+}
+
+module.exports = new Tabelas
