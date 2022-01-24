f83e096d7467eaccb17fa2c7918488e6ec3cd45b
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 17:41:52 -0400

    implementando metodo pra salvar no banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,13 @@
+const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) =>
         res.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Vocês está na rota de atendimentos e está usando o POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post Atendimento')
     })
 
 }
