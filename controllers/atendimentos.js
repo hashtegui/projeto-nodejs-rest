ce3740e45e4e7b4fdf8844537e011da957bcf9c3
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-01-24 14:01:38 -0400

    criando rota post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,4 +1,7 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) =>
+        res.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => res.send('Vocês está na rota de atendimentos e está usando o POST'))
 
 }
